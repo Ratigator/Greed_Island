0540408657e7333af8876a1a74481f8ac09a9a24
Goob <zteaster@my.chattanoogastate.edu>
2023-05-30 22:13:49 -0400

    Update

---
+++ src/main/java/net/goob/greedisland/GreedIsland.java
@@ -0,0 +1,15 @@
+package net.goob.greedisland;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GreedIsland implements ModInitializer {
+	public static final String MOD_ID = "greedisland";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/goob/greedisland/GreedIslandClient.java
@@ -0,0 +1,10 @@
+package net.goob.greedisland;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GreedIslandClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
